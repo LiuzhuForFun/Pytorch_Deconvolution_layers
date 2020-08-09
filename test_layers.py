7123aad9a84a3c8c63b0bc4f94a0eab9361b0491
zhu.liu <liuzhu_dlut@mail.dlut.edu.cn>
2020-08-09 17:53:40 +0800

    The first submition

--- test_layers.py
+++ test_layers.py
@@ -1,10 +1,3 @@
-# Pytorch_Deconvolution_layers
- Codes about the implements of Deconvolution Layers
-
-The test codes are following:
-
-```python
-
 import torch
 from image_restoration_v3.layer import ISubLayer
 from skimage import transform
@@ -27,13 +20,4 @@ print(np.shape(image_tensor))
 layer = ISubLayer()
 out = layer(image_tensor,image_tensor,torch.Tensor(test_k),torch.Tensor([0.05]))
 torchvision.utils.save_image(out[:1], 'test_deblur.png')
-```
-The test results
-
-![png](7.png "blurred image")
-![png](test_deblur.png "test_delbur image")
-
-About the tensorflow code, you can link 
-
-[DCT2D_tensorflow](https://github.com/Heyi007/tensorflow-DCT2d)
 
